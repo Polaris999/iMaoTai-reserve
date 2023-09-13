59ae1f8cc52c15765e63338fac9e2b746c89b197
xiaokai@jingrizf.com <Administrator@xiaokai.tech.jingrizf.cn>
2023-09-13 09:47:06 +0800

    update

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ PUSH_TOKEN = os.environ.get("PUSHPLUS_KEY")
 *********** 地图配置 ***********
 获取地点信息,这里用的高德api,需要自己去高德开发者平台申请自己的key
 '''
-AMAP_KEY = '79fa1f1a36c479f206a9c0adc98707fb'
+AMAP_KEY =  os.environ.get("GAODE_KEY")
 
 
 '''
